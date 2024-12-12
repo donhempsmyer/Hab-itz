39fbcea3c2b05bda67b2eca5c150f6fcb2875a78
James Carter <98671343+Zoro2131@users.noreply.github.com>
2024-12-11 18:58:34 -0600

    Added: LogIn Fragment textbox_border.xml(used to add a border to around a text box) Icons used for the login linked the LoginScreenFragment into the nav_graph.xml the Habgreen color into the colors.xml so we have it there added UI elements to the Login fragment_login.xml

---
+++ app/src/main/java/com/example/mytestapplication2/LoginScreenFragment.kt
@@ -0,0 +1,34 @@
+package com.example.mytestapplication2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+
+
+class LoginScreenFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+
+        val rootView = inflater.inflate(R.layout.fragment_login, container, false)
+
+        val imageView: ImageView = rootView.findViewById(R.id.login_icon)
+
+
+        val originalBitmap = BitmapFactory.decodeResource(resources, R.drawable.habitz_icon)
+        val resizedBitmap = Bitmap.createScaledBitmap(originalBitmap, 150, 120, false)
+        imageView.setImageBitmap(resizedBitmap)
+
+
+        return rootView
+    }
+
+}
