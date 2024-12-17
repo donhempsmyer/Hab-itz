b26da3b39016cb564761b00fd61efe1b916fa332
Tav0nes <gustavobastosaraujo@yahoo.com.br>
2024-12-16 22:05:59 -0500

    New structure push into dev

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("kotlin-kapt")
 }
 
 android {
@@ -50,4 +51,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    kapt(libs.androidx.room.compiler)
+    androidTestImplementation(libs.androidx.room.testing)
 }
---
+++ app/src/main/java/com/example/mytestapplication2/ui/fragments/createhabit/CreateHabitScreen.kt
@@ -0,0 +1,13 @@
+package com.example.mytestapplication2.ui.fragments.createhabit
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.mytestapplication2.R
+
+class CreateHabitScreen : Fragment(R.layout.fragment_create_habit_screen) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
