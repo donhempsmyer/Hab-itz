1426a1d9cff7aa82ab0fc1bd430a0696fea4c925
James Carter <98671343+Zoro2131@users.noreply.github.com>
2024-12-30 17:55:07 -0600

    move the hamburgericon to the toolbar can not get the nav icon to change(I might have to make a custom toolbar for this) removed the 2 menus that where not in use removed the titles for each fragment

--- app/src/main/java/com/example/mytestapplication2/MainActivity.kt
+++ app/src/main/java/com/example/mytestapplication2/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.example.mytestapplication2
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.PopupMenu
+import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
 import com.example.mytestapplication2.databinding.ActivityMainBinding
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,25 +26,63 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
+
+
+        // assigning id of the toolbar to a var
+        val mainToolbar = findViewById<MaterialToolbar>(R.id.toolbar)
+
+        //using toolbar as  ActionBar
+        setSupportActionBar(mainToolbar)
+
+        // Set the hamburger icon as the navigation icon
+        binding.toolbar.setNavigationIcon(R.drawable.hamburgericon)
+
+//        //doesn't work(should work)
+//        // Sets the nav Icon to the hamburger_icon
+//        mainToolbar.setNavigationIcon(R.drawable.hamburger_icon)
+
+//        //doesn't work lol
+//        // Disable the default back arrow if needed (no need if using hamburger icon)
+//        supportActionBar?.setDisplayHomeAsUpEnabled(false)
+
 
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
+        // Handle the click on the hamburger icon
+        binding.toolbar.setNavigationOnClickListener { view ->
+            val hamburgerPopupMenu = PopupMenu(this, view)
+            hamburgerPopupMenu.menuInflater.inflate(R.menu.hamburger_menu, hamburgerPopupMenu.menu)
+
+            hamburgerPopupMenu.setOnMenuItemClickListener { item ->
+                when (item.itemId) {
+                    R.id.home_navBtn -> {
+                        //Navigate to the MainScreenFragment
+                        findNavController(R.id.nav_host_fragment_content_main).navigate(R.id.action_CurrentDayFragment_to_MainScreenFragment)
+                        true
+                    }
+                    R.id.habitz_navBtn -> {
+                        // Navigate to the HabitzScreenFragment
+                        findNavController(R.id.nav_host_fragment_content_main).navigate(R.id.action_MainScreenFragment_to_HabitzScreenFragment)
+                        true
+                    }
+                    R.id.journal_navBtn -> {
+                        // Navigate to the Journal fragment
+                       // findNavController(R.id.nav_host_fragment_content_main).navigate(R.id.action_MainScreenFragment_to_JournalFragment)
+                        true
+                    }
+                    R.id.scheduler_navBtn -> {
+                        // Navigate to the CurrentDayFragment
+                        findNavController(R.id.nav_host_fragment_content_main).navigate(R.id.action_MainScreenFragment_to_CurrentDayFragment)
+                        true
+                    }
+                    else -> false
+                }
+            }
+
+            hamburgerPopupMenu.show()
+        }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
-    }
 }
--- app/src/main/java/com/example/mytestapplication2/ui/fragments/mainScreen/MainScreenFragment.kt
+++ app/src/main/java/com/example/mytestapplication2/ui/fragments/mainScreen/MainScreenFragment.kt
@@ -28,70 +28,45 @@ class MainScreenFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentMainScreenBinding.inflate(inflater, container, false)
-
-        // Finds the hamburger icon
-        val hamburgerIcon: ImageView = binding.hamburgerIcon
-
-        // Sets the click listener for the hamburger icon
-        hamburgerIcon.setOnClickListener { view ->
-            // Creates the popup menu
-
-            val hamburgerPopupMenu = PopupMenu(requireContext(), hamburgerIcon)
-            hamburgerPopupMenu.menuInflater.inflate(R.menu.hamburger_menu, hamburgerPopupMenu.menu)
-
-            hamburgerPopupMenu.setOnMenuItemClickListener { item: MenuItem ->
-                when (item.itemId) {
-
-                    R.id.habitz_navBtn -> {
-                        // Navigate to the HabitzScreenFragment
-                        findNavController().navigate(R.id.action_MainScreenFragment_to_HabitzScreenFragment)
-                        true
-                    }
-                    R.id.journal_navBtn -> {
-                        // Navigate to the Journal fragment
-                        //findNavController().navigate(R.id.action_MainScreenFragment_to_JournalFragment)
-                        true
-                    }
-                    R.id.scheduler_navBtn -> {
-                        // Navigate to the CurrentDayFragment
-                        findNavController().navigate(R.id.action_MainScreenFragment_to_CurrentDayFragment)
-                        true
-                    }
-                    else -> false
-                }
-            }
-
-            hamburgerPopupMenu.show()
-        }
+//
+//        // Finds the hamburger icon
+//        val hamburgerIcon: ImageView = binding.hamburgerIcon
+//
+////        // Sets the click listener for the hamburger icon
+////        hamburgerIcon.setOnClickListener { view ->
+////            // Creates the popup menu
+////
+////            val hamburgerPopupMenu = PopupMenu(requireContext(), hamburgerIcon)
+////            hamburgerPopupMenu.menuInflater.inflate(R.menu.hamburger_menu, hamburgerPopupMenu.menu)
+////
+////            hamburgerPopupMenu.setOnMenuItemClickListener { item: MenuItem ->
+////                when (item.itemId) {
+////
+////                    R.id.habitz_navBtn -> {
+////                        // Navigate to the HabitzScreenFragment
+////                        findNavController().navigate(R.id.action_MainScreenFragment_to_HabitzScreenFragment)
+////                        true
+////                    }
+////                    R.id.journal_navBtn -> {
+////                        // Navigate to the Journal fragment
+////                        //findNavController().navigate(R.id.action_MainScreenFragment_to_JournalFragment)
+////                        true
+////                    }
+////                    R.id.scheduler_navBtn -> {
+////                        // Navigate to the CurrentDayFragment
+////                        findNavController().navigate(R.id.action_MainScreenFragment_to_CurrentDayFragment)
+////                        true
+////                    }
+////                    else -> false
+////                }
+////            }
+////
+////            hamburgerPopupMenu.show()
+////        }
 
         return binding.root
     }
 
-//    // Function to replace the current fragment in the container
-//    //I(james) need a better understanding of this/ need more research
-//    private fun addFragmentToContainer(fragment: Fragment) {
-//        try {
-//            // Log to check if the function is being called
-//            //error checking via logcat
-//            Log.d("MainScreenFragment", "Replacing fragment with ${fragment::class.java.simpleName}")
-//
-//            // Check if the fragment container exists
-//            val fragmentContainer = view?.findViewById<View>(R.id.fragment_container)
-//            if (fragmentContainer == null) {
-//                //error checking
-//                Log.e("MainScreenFragment", "Fragment container is null!")
-//                return
-//            }
-//
-//            parentFragmentManager.beginTransaction()  // Starts the transaction
-//                .replace(R.id.fragment_container, fragment)  // Replaces the fragment
-//                .addToBackStack(null)  //Adds the transaction to back stack
-//                .commit()  // Commit the transaction to apply the changes
-//        } catch (e: Exception) {
-//            // Logs any errors that occur during the fragment transaction
-//            Log.e("MainScreenFragment", "Error during fragment transaction: ${e.localizedMessage}")
-//        }
-//    }
 
     override fun onDestroyView() {
         super.onDestroyView()
