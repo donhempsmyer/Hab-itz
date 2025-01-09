7d4f23d69645cb47aa4006f808272e085cfda40c
donhempsmyer <spencerhempsmyer@gmail.com>
2025-01-08 18:08:14 -0600

    Continuing work on the recycler view user input.

--- app/src/main/java/com/example/mytestapplication2/ui/fragments/currentDay/CurrentDayFragment.kt
+++ app/src/main/java/com/example/mytestapplication2/ui/fragments/currentDay/CurrentDayFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mytestapplication2.data.models.TimeCell
@@ -14,15 +15,22 @@ import com.example.mytestapplication2.databinding.FragmentCurrentDayBinding
 class CurrentDayFragment : Fragment() {
 
     private lateinit var binding: FragmentCurrentDayBinding
+    lateinit var eventText: EditText
+
+
+    var userTextList = ArrayList<EditText>(48)
+
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
         binding = FragmentCurrentDayBinding.inflate(inflater, container, false)
         return binding.root
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -30,18 +38,30 @@ class CurrentDayFragment : Fragment() {
 
         populateDay()
 
-        val FragmentCurrentDay = this
+
+        val CurrentDayFragment = this
         binding.mHalfHourRecyclerView.apply {
             layoutManager = LinearLayoutManager(context)
             adapter = TimeAdapter(timeList)
-
         }
+
     }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+
+
 
     private fun populateDay() {
         val semiHourlyList: ArrayList<String> = ArrayList<String>()
+
+
         semiHourlyList.add("12:00:00 AM")
+
         semiHourlyList.add("12:30:00 AM")
+
 
         var hourVar = 1
         while (hourVar < 12) {
@@ -61,11 +81,11 @@ class CurrentDayFragment : Fragment() {
 
         }
         for (i in semiHourlyList.indices) {
-            timeList.add (TimeCell(
 
+            var timeCell = (TimeCell(
                 time = semiHourlyList[i],
-                timeContent = "")
-            )
+                timeContent = ""))
+            timeList.add(timeCell)
         }
     }
 
